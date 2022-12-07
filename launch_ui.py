a2fb1ae6235074fd148d0a8f0ac17a636fd34942
ZeroHackz <15729496+ZeroHackz@users.noreply.github.com>
2022-12-07 19:07:42 +0100

    chore(cleanup): cleaning up main branch to only working features.
    chore(cleanup): cleaning up main branch to only working features.

--- launch_ui.py
+++ launch_ui.py
@@ -15,15 +15,9 @@ user_has_gpu = False;
 #configs
 model_path, config_path, model_item = tts_models_manager.fetchLanguageModel("en")
 synthesizer = Synthesizer(model_path, config_path, None, None, None)
-
+#paths and directories
 if not os.path.exists(audio_dir):
     os.makedirs(audio_dir)
-    
-def TTSPersonalModels(name):
-    #unsopported for now
-    #TODO: add support for personal models
-    return f"Hello, {name}!"
-
 #SUPPORTED FEATURES
 def coquiDefaultModel(givenText):
     localTimestamp = str(int(time.time()))
@@ -34,31 +28,6 @@ def coquiDefaultModel(givenText):
     synthesizer.save_wav(enhanced, filepath) # save the wav file to the file path
     wav_path = audio_dir + "/" + str(localTimestamp) + ".wav"
     return wav_path
-
-#UNSUPPORTED FEATURES
-def coquiTrainiModelOnSingleAudio(givenAudio):
-    localTimestamp = str(int(time.time()))
-    filename = str(localTimestamp) + ".wav"    
-    wavs = synthesizer.tts(givenAudio)
-    # create the file path for the wav file
-    filepath = os.path.join(audio_dir, filename)
-    # save the wav file to the file path
-    
-    wavs = synthesizer.normalize_wav(wavs)
-    synthesizer.save_wav(wavs, filepath)
-    #unsupoorted for now
-    #TODO: add support for batch processing
-    
-    return audio_dir + "/" + str(localTimestamp) + ".wav"
-
-def coquiTrainiModelOnBatchAudio(input_batch_directory, output_batch_directory):
-    localTimestamp = str(int(time.time()))
-    filename = str(localTimestamp) + ".wav"    
-    filepath = os.path.join(audio_dir, filename)
-    #unsupoorted for now
-    #TODO: add support for batch processing
-    return audio_dir + "/" + str(localTimestamp) + ".wav"
-
 # Create a gradio interface for the greeting function
 coqui_ui_interface_000 = gr.Interface(
     fn=coquiDefaultModel, # the function to use
@@ -68,21 +37,7 @@ coqui_ui_interface_000 = gr.Interface(
     title="Coqui TTS", # the title of the UI
     description="Enter your text and get a audio file message" # brief description of the function
 )
-coqui_ui_interface_001 = gr.Interface(
-    fn=coquiSingleAudio, # the function to use
-    inputs="audio", # the input type # the input type
-    outputs="audio", # the output type
-    title="Coqui TTS", # the title of the UI
-    description="Enter your text and get a audio file message" # brief description of the function
-)
-coqui_ui_interface_002 = gr.Interface(
-    fn=coquiBatchAudio, # the function to use
-    inputs="text", # the input type # the input type
-    outputs="audio", # the output type
-    title="Coqui TTS", # the title of the UI
-    description="Enter your text and get a audio file message" # brief description of the function
-)
-coqui_ui = gr.TabbedInterface([coqui_ui_interface_000, coqui_ui_interface_001, coqui_ui_interface_002], ["Text-to-speech", "Train Model Audio", "Train model on Batch"])
+coqui_ui = gr.TabbedInterface([coqui_ui_interface_000,], ["Text-to-speech"])
 
 # Display the UI
 coqui_ui.launch(inline=True)
