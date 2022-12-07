a21c0145dd815b752d5d76d9dc1531dbffa7e895
ZeroHackz <15729496+ZeroHackz@users.noreply.github.com>
2022-12-07 18:10:34 +0100

    chore(cleanup): code clean up prepping for branching to feature branches.
    chore(cleanup): code clean up prepping for branching to feature branches.

--- launch_ui.py
+++ launch_ui.py
@@ -17,9 +17,12 @@ if not os.path.exists(audio_dir):
     os.makedirs(audio_dir)
     
 def TTSPersonalModels(name):
-  return f"Hello, {name}!"
-    
-def coquiDefaultModel(givenText = '你好吗？我很好'):
+    #unsopported for now
+    #TODO: add support for personal models
+    return f"Hello, {name}!"
+
+#SUPPORTED FEATURES
+def coquiDefaultModel(givenText = 'Hey, im using my own webUI for Coqui TTS!'):
     localTimestamp = str(int(time.time()))
     filename = str(localTimestamp) + ".wav"    
     wavs = synthesizer.tts(givenText)
@@ -27,6 +30,7 @@ def coquiDefaultModel(givenText = '你好吗？我很好'):
     synthesizer.save_wav(wavs, filepath) # save the wav file to the file path
     return audio_dir + "/" + str(localTimestamp) + ".wav"
 
+#UNSUPPORTED FEATURES
 def coquiSingleAudio(givenAudio):
     localTimestamp = str(int(time.time()))
     filename = str(localTimestamp) + ".wav"    
@@ -34,25 +38,17 @@ def coquiSingleAudio(givenAudio):
     # create the file path for the wav file
     filepath = os.path.join(audio_dir, filename)
     # save the wav file to the file path
-    synthesizer.save_wav(wavs, filepath)    
-    #cmd = "$ tts --text \"{}\" --model_name \"<model_type>/<language>/<dataset>/<model_name>\" --{}/{}.wav".format(givenText, audio_dir, str(localTimestamp))
-    ## save wav to audio_dir
-    #execute the command
-    #subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    synthesizer.save_wav(wavs, filepath)
+    #unsupoorted for now
+    #TODO: add support for batch processing
     return audio_dir + "/" + str(localTimestamp) + ".wav"
 
 def coquiBatchAudio(input_batch_directory, output_batch_directory):
     localTimestamp = str(int(time.time()))
     filename = str(localTimestamp) + ".wav"    
-    #wavs = synthesizer.tts(givenAudio)
-    # create the file path for the wav file
     filepath = os.path.join(audio_dir, filename)
-    # save the wav file to the file path
-    #synthesizer.save_wav(wavs, filepath)    
-    #cmd = "$ tts --text \"{}\" --model_name \"<model_type>/<language>/<dataset>/<model_name>\" --{}/{}.wav".format(givenText, audio_dir, str(localTimestamp))
-    ## save wav to audio_dir
-    #execute the command
-    #subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    #unsupoorted for now
+    #TODO: add support for batch processing
     return audio_dir + "/" + str(localTimestamp) + ".wav"
 
 # Create a gradio interface for the greeting function
@@ -79,5 +75,6 @@ coqui_ui_interface_002 = gr.Interface(
     description="Enter your text and get a audio file message" # brief description of the function
 )
 coqui_ui = gr.TabbedInterface([coqui_ui_interface_000, coqui_ui_interface_001, coqui_ui_interface_002], ["Text-to-speech", "Train Model Audio", "Train model on Batch"])
+
 # Display the UI
 coqui_ui.launch(inline=True)
